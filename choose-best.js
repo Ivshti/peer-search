b31252eae053aa0e1522d27e539b8d2e8d9034a7
Ivo Georgiev <ivo@linvo.org>
2013-07-07 00:20:47 +0300

    choose-best now returns the peers/hash of the best torrent

--- choose-best.js
+++ choose-best.js
@@ -9,7 +9,7 @@ var async = require("async");
 var DHTs = [],
     start = Date.now();
 
-async.each(
+async.map(
     [
         "99cf7a5ae561b7a55072eb07645277b6df0ef902",
         "2b96d97e1de415f0f308f1cf70e9a18567f4a7f9",
@@ -25,11 +25,12 @@ async.each(
         dht.on("peer", function(peer) {
             peers.push(peer);
             if (peers.length == 100) DHTs.forEach(function(d) { if (d != dht) { d.stop(); d.callback() } });
-            if (peers.length == 300) callback();
+            if (peers.length == 300) callback(null, { hash: hash, peers: peers });
         });
     },
     function(err, res)
     {
-        console.log("Ready after", Date.now()-start)
+        console.log("Ready after", Date.now()-start);
+        console.log(res);
     }
 );
