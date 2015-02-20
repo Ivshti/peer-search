099fea4ebf89c5c9d364adc8ce70cc9ecee4069d
Ivo Georgiev <ivo@linvo.org>
2015-02-20 18:42:48 +0200

    Control logic is now more or less there

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 var DHT = require("./dht");
+var Pump = require("./pump");
 var events = require("events");
 
 module.exports = function peerSearch(sources, swarm, options) 
 {
-	var sources = sources.map(function setupSource() {
-		
-		//TODO
-		//ev.
-	});
+	var self = this;
 
-	var ev = new events.EventEmitter();
-	ev.setMaxListeners(0); // unlimited power
+	var sources = sources.map(function setupSource(src) {
+		if (src.match("^dht:")) return new DHT(new Buffer(src.split(":")[1],"hex"), options);
+		if (src.match("^pump:")) return new Pump(src.split(":")[1]);
+	}).filter(function(x){ return x });
 
-	this.run = function() { sources.forEach(function(x) { x.run() }) };
-	this.pause = function() { sources.forEach(function(x) { x.pause() }) };
+	var running = false;
+	this.run = function() { running=true; sources.forEach(function(x) { x.run() }) };
+	this.pause = function() { running=false; sources.forEach(function(x) { x.pause() }) };
 
 	this.run(); // All sources should be initialized paused
+
+	var update = function() {
+		var len = Object.keys(swarm._peers).length;
+		if (options.hasOwnProperty("min") && (len < options.min) && !running) return self.run();
+		if (options.hasOwnProperty("max") && (len > options.max) && running) return self.pause();
+	};
+	swarm.on("wire", update);
+	swarm.on("wire-disconnect", update);
+
 };
