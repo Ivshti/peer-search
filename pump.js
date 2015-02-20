49446cec0c35a23f0bd6ad58d5cfc3a2d08e8d06
Ivo Georgiev <ivo@linvo.org>
2015-02-20 19:15:33 +0200

    pump: implemented

--- pump.js
+++ pump.js
@@ -1,5 +1,19 @@
+var request = require("request");
+var byline = require("byline");
+var EventEmitter = require('events').EventEmitter;
+
+
 module.exports = function Pump(url)
 {
+	var self = this;
+	EventEmitter.call(this);
 
+	this.run = function() {
+		 byline(request(url)).on("data", function(d) { self.emit("peer",d.toString()) });
+	};
 
+	this.pause = function() {
+		// No point
+	};
 };
+Pump.prototype.__proto__ = EventEmitter.prototype;
