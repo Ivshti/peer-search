f3607c616a8c5e8621bd26f4df841c05ab4660c4
Ivo Georgiev <ivo@linvo.org>
2015-02-20 16:34:24 +0200

    committing more backbone work before doing DHT re-think

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 
-var DHT = require('./');
+var DHT = require('./dht');
 var async = require('async');
 
 async.eachSeries(
@@ -7,7 +7,7 @@ async.eachSeries(
     [
         'e756b6ed7f1f647db2ea7e153e2fdc6226218a1f',
      //   'e756b6ed7f1f647db2ea7e153e2fdc6226218a1f',
-        '948cd498ab5acdc0a61dee8b012eb93ae231b2ff',
+     //   '948cd498ab5acdc0a61dee8b012eb93ae231b2ff',
     ], 
     function(hash, callback) {
         var dht = new DHT(new Buffer(hash, 'hex'));
@@ -15,7 +15,6 @@ async.eachSeries(
 
         var peers = [], start = Date.now(), i = 0;
         dht.on('peer', function(peer) {
-            //console.log(hash, ++i); // DEBUG
             peers.push(peer);
             
             if (peers.length != 300) return;
--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 var DHT = require("./dht");
 var events = require("events");
 
-function setupSource(source)
-{
-
-};
-
-
 module.exports = function peerSearch(sources, options) 
 {
+	var sources = sources.map(function setupSource() {
+		//TODO
+		//ev.	
+	});
+
 	var ev = new events.EventEmitter();
 	ev.setMaxListeners(0); // unlimited power
 	var queue = [];
 
 	this.shift = function(cb) {
+		if (!queue.length) { /* TODO */return };
 		// TODO
 	};
 	this.length = function() { return queue.length };
+
+	this.run = function() { sources.forEach(function(x) { x.run() }) };
+	this.pause = function() { sources.forEach(function(x) { x.pause() }) };
+
+	this.run(); // All sources should be initialized paused
 };
