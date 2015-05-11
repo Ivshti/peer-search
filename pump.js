88e4ecef2898fcddfbb4a6b68dbcdfb3a1b1c47a
Ivo Georgiev <ivo@linvo.org>
2015-05-11 13:33:41 +0300

    Refresh pump source periodically unless paused

--- pump.js
+++ pump.js
@@ -1,19 +1,24 @@
 var request = require("request");
 var byline = require("byline");
 var EventEmitter = require("events").EventEmitter;
+var async = require("async");
 
+var REFRESH_INTERVAL = 25*1000;
 
 function Pump(url)
 {
 	var self = this;
 	EventEmitter.call(this);
 
+	var refreshInterval = null;
+	
 	this.run = function() {
 		 byline(request(url)).on("data", function(d) { self.emit("peer",d.toString()) });
+		 if (!refreshInterval) refreshInterval = setInterval(function() { self.run()  }, REFRESH_INTERVAL);
 	};
 
 	this.pause = function() {
-		// No point
+		clearInterval(refreshInterval);
 	};
 };
 Pump.prototype.__proto__ = EventEmitter.prototype;
